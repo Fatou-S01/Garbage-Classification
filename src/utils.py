98a76b6d65853e64b1cfc81ef7b28f6702e237e4
nflagnane <lfatou@ept.sn>
2024-08-07 22:32:31 +0000

    Training and Testing

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import mlflow
 import mlflow.keras
 import pandas as pd
 import os
+import cv2
+import numpy as np
 
 def plot_confusion_matrix(conf_matrix, title):
     """ Plot the confusion matrix as a heatmap. """
@@ -40,13 +42,22 @@ def save_best_model_by_val_accuracy(experiment_name, save_directory):
 
     # Charger le modèle du meilleur run
     best_run_id = best_run.run_id
-    model_uri = f"runs:/{best_run_id}/model"
+    model_name = best_run['params.model_name']  
+    model_uri = f"runs:/{best_run_id}/model_{model_name}"
     model = mlflow.keras.load_model(model_uri)
 
     # Créer le répertoire de sauvegarde s'il n'existe pas
     os.makedirs(save_directory, exist_ok=True)
-    save_path = os.path.join(save_directory, "best_model.h5")
+    save_path = os.path.join(save_directory, f"best_model.h5")
 
     # Enregistrer le modèle localement
     model.save(save_path)
     print(f"Best model saved to {save_path}")
+
+#Save images to the specified directory structure.
+def save_images(images, labels, base_path):
+    for i, (img, label) in enumerate(zip(images, labels)):
+        label_dir = os.path.join(base_path, str(np.argmax(label)))
+        os.makedirs(label_dir, exist_ok=True)
+        img_path = os.path.join(label_dir, f'image_{i}.png')
+        cv2.imwrite(img_path, img)
